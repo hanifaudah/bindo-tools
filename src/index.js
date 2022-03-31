08951dcf1120ac4bf39ec83012097cb2575ae988
hanifaudah <hanifarkanaudah@gmail.com>
2022-03-31 11:47:18 +0700

    fix: remove basename from index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.REACT_APP_BASE_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
